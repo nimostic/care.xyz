f495badf77b9c1f5ee2505200ed4112da19f6a79
Abu Nayeem Riyad <rionimo5@gmail.com>
2026-03-24 17:34:55 +0600

    output token added

--- src/proxy.js
+++ src/proxy.js
@@ -2,7 +2,13 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 const privateRoute = ["/booking", "/my-booking"];
 export async function proxy(req) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const token = await getToken({
+  req,
+  secret: process.env.NEXTAUTH_SECRET,
+  cookieName: process.env.NODE_ENV === "production"
+    ? "__Secure-next-auth.session-token"
+    : "next-auth.session-token",
+});
   console.log(token);
   const isAuthenticated = Boolean(token);
   const reqPath = req.nextUrl.pathname;
